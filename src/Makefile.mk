d33a694f6f9ef69af37d35a116bb49ba393e076d
Bart Bilos <squantmuts@gmail.com>
2019-03-04 20:41:44 +0100

    added generic PC build environment

---
+++ src/Makefile.mk
@@ -0,0 +1,10 @@
+# project settings
+BIN_NAME = squantorMinUnitCexample
+C_SOURCES = src/main.c
+CXX_SOURCES =
+S_SOURCES =
+INCLUDES = -Iinc
+COMPILE_C_FLAGS += -std=gnu11 -Wall -Wextra
+COMPILE_CXX_FLAGS += -std=c++17 -Wall -Wextra
+COMPILE_ASM_FLAGS += -c -x assembler-with-cpp
+LINK_FLAGS += -flto -Xlinker --gc-sections -Xlinker -print-memory-usage
