0f3e38d0bf26221b005d17c33f39210d9b8a65a3
Bart Bilos <squantmuts@gmail.com>
2019-04-03 22:29:13 +0200

    updated buildsystem

--- tests/platforms/PC.mk
+++ tests/platforms/PC.mk
@@ -1,11 +1,8 @@
 # platform settings for PC's with GCC and libc
 TOOLCHAIN_PREFIX =
 C_SOURCES += src/main_PC.c
-COMPILE_C_FLAGS += -std=gnu11 -Wall -Wextra
-COMPILE_CXX_FLAGS += -std=c++17 -Wall -Wextra
-COMPILE_ASM_FLAGS += -c -x assembler-with-cpp
-#LINK_FLAGS += -Wl,--gc-sections -Wl,-print-memory-usage -Wl,--trace
-LINK_FLAGS += --gc-sections -print-memory-usage
-PLATFORM_OBJECTS =  /usr/lib/x86_64-linux-gnu/Scrt1.o /usr/lib/x86_64-linux-gnu/crti.o /usr/lib/gcc/x86_64-linux-gnu/7/crtbeginS.o /usr/lib/gcc/x86_64-linux-gnu/7/crtendS.o /usr/lib/x86_64-linux-gnu/crtn.o
-LIBS += -L /usr/lib/x86_64-linux-gnu -L /usr/lib/gcc/x86_64-linux-gnu/7/ -lgcc -lgcc_eh -lc
-LINKER = ld
+COMPILE_C_FLAGS +=
+COMPILE_CXX_FLAGS +=
+COMPILE_ASM_FLAGS +=
+LINK_FLAGS += -Wl,--gc-sections -Wl,-print-memory-usage
+LINKER = g++
--- tests/src/Makefile.mk
+++ tests/src/Makefile.mk
@@ -1,7 +1,33 @@
+# update per change V0006
+
+# project name
+BIN_NAME = template_microcontroller
+PLATFORM ?= PC
+
 # project settings
-BIN_NAME = squantorMinUnitCexample
 C_SOURCES += src/tests1.c
 CXX_SOURCES +=
 S_SOURCES +=
 INCLUDES += -Iinc -I../inc
+DEFINES +=
+ALIBS +=
+RLIBS +=
+DLIBS +=
+ALIBDIR +=
+RLIBDIR +=
+DLIBDIR +=
+COMPILE_C_FLAGS = -std=gnu11 -Wall -Wextra -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections
+COMPILE_CXX_FLAGS = -std=c++17 -Wall -Wextra -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions
+COMPILE_ASM_FLAGS = -c -x assembler-with-cpp
+LINK_FLAGS +=
 
+#custom build rules
+pre-clean:
+
+pre-release:
+
+pre-debug:
+
+#project hardware specific commands
+
+.PHONY: pre-clean pre-release pre-debug
