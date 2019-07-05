ea661bef985a1de2789046693c0cf40364dddbc7
Bart Bilos <squantmuts@gmail.com>
2019-07-05 12:18:24 +0200

    some changes to build system

--- tests/src/Makefile.mk
+++ tests/src/Makefile.mk
@@ -1,8 +1,11 @@
 # update per change V0006
 
 # project name
-BIN_NAME = template_microcontroller
+BIN_NAME = squantorMinUnitTemplate
 PLATFORM ?= PC
+
+#include PLATFORM specific settings/rules
+include src/$(PLATFORM).mk
 
 # project settings
 C_SOURCES += src/tests1.c
@@ -20,14 +23,3 @@ COMPILE_C_FLAGS = -std=gnu11 -Wall -Wextra -Wno-main -fno-common -c -fmessage-le
 COMPILE_CXX_FLAGS = -std=c++17 -Wall -Wextra -Wno-main -fno-common -c -fmessage-length=0 -fno-builtin -ffunction-sections -fdata-sections -fno-rtti -fno-exceptions
 COMPILE_ASM_FLAGS = -c -x assembler-with-cpp
 LINK_FLAGS +=
-
-#custom build rules
-pre-clean:
-
-pre-release:
-
-pre-debug:
-
-#project hardware specific commands
-
-.PHONY: pre-clean pre-release pre-debug
