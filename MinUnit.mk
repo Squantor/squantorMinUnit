e444118bf4681152fa225e08032b7a8e4352acba
Squantor <squantmuts@gmail.com>
2020-08-14 12:05:16 +0200

    added minunit Makefile settings

---
+++ MinUnit.mk
@@ -0,0 +1,13 @@
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2020-2020 Bart Bilos
+# For conditions of distribution and use, see LICENSE file
+
+
+# MinUnit library settings
+#
+# Version: 20200814
+
+# library files and includes
+INCLUDES += -IMinUnit/inc
+
