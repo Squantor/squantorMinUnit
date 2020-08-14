9236a8fed6227ff36c69fb85280521a0985f899c
Squantor <squantmuts@gmail.com>
2020-08-14 12:59:58 +0200

    added doxygen documentation

--- inc/MinUnit.hpp
+++ inc/MinUnit.hpp
@@ -4,10 +4,14 @@
  * Copyright (c) 2020-2020 Bart Bilos
  * For conditions of distribution and use, see LICENSE file
  */
-/**
+/** 
+ * \file MinUnit.hpp
+ * 
+ * Minimal C++ unittesting framework
+ * 
  * Minimal C++ unittesting framework, based on:
  * https://github.com/siu/minunit
- */
+ */ 
 #ifndef MINUNIT_HPP
 #define MINUNIT_HPP
 
@@ -20,60 +24,110 @@
 #endif
 
 /**
- *  Macro to wrap a safe define block
- *  
- *  Helper macro to create a safe block to be used in other C style macros
- *  @param[in]  block   Block of statements that will be wrapped in a ```do{ }while(0)``` block.
+ * \brief Macro to wrap a safe define block
+ * 
+ * Helper macro to create a safe block to be used in other C style macros
+ * @param[in]  block   Block of statements that will be wrapped in a ```do{ }while(0)``` block.
  */
 #define MU__SAFE_BLOCK(block) do {\
     block\
 } while(0)
 
+/**
+ * \brief Macro to generate the global state for all the tests
+ * 
+ * Generates the global state for all tests, these are all the counters like tests run, 
+ * tests failed and amount of test assertions executed.
+ */
 #define MU_TEST_GLOBAL_STATE \
     int minunitRun; /*!< Total tests run, needs to be gobally defined */\
     int minunitFailures; /*!< Total tests failed, needs to be globally defined */\
     int minunitAsserts; /*!< Total test asserts executed, needs to be globally defined */\
 
+/**
+ * \brief Macro to generate the state for the current test suite
+ * 
+ * Generates the local state for the test suite, these are setup and teardown functions and
+ * the running test state.
+ */
 #define MU_TEST_TEST_STATE \
     static int minunitStatus = 0; /*!< Test suite local status variable */\
     static void (*minunitSetup)(void) = NULL; /*!< Test suite local function pointer to test setup */\
     static void (*minunitTeardown)(void) = NULL; /*!< Test suite local function pointer to test teardown */\
 
-extern int minunitRun; /*!< Total tests run, needs to be gobally defined */
-extern int minunitFailures; /*!< Total tests failed, needs to be globally defined */
-extern int minunitAsserts; /*!< Total test asserts executed, needs to be globally defined */
+
+extern int minunitRun; /*!< Total tests run, needs to be defined with MU_TEST_GLOBAL_STATE */
+extern int minunitFailures; /*!< Total tests failed, needs to be defined with MU_TEST_GLOBAL_STATE */
+extern int minunitAsserts; /*!< Total test asserts executed, needs to be defined with MU_TEST_GLOBAL_STATE */
 
 /**
- * Creates a test suite entry
+ * \brief Creates a test suite entry
  * 
- * Used to create a test suite for a bunch of tests, example definition:
+ * Used to create a test suite for a bunch of tests, example:
  * ```
  * MU_TEST_SUITE(mysuite)
  * {
+ *     MU_SUITE_CONFIGURE(&myTestSetup, &myTestTeardown);
+ *     MU_RUN_TEST(mytest);
  * }
  * ```
  */
 #define MU_TEST_SUITE(suiteName) static void suiteName(void)
 
 /**
- *  
+ * \brief Configures the test suite
+ * 
+ * Configures the tests suite with the setup and teardown functions.
+ * Needs to be configured before each test.
+ * ```
+ * MU_TEST_SUITE(mysuite)
+ * {
+ *     MU_SUITE_CONFIGURE(&myTestSetup, &myTestTeardown);
+ *     MU_RUN_TEST(mytest);
+ * }
+ * ```
+ * @param[in] setupFun Function pointer to the test setup function
+ * @param[in] teardownFun Function pointer to the test teardown function
  */
-#define MU_TEST(methodName) static void methodName(void)
+#define MU_SUITE_CONFIGURE(setupFun, teardownFun) MU__SAFE_BLOCK(\
+    minunitSetup = setupFun;\
+    minunitTeardown = teardownFun;\
+)
 
-/*  Run test suite and unset setup and teardown functions */
+/**
+ * \brief Executes a test suite
+ * 
+ * Executes a defined test suite, clears the setup and teardown afterwards.
+ * ```
+ * MU_TEST_SUITE(mysuite)
+ * {
+ *     MU_SUITE_CONFIGURE(&myTestSetup, &myTestTeardown);
+ *     MU_RUN_TEST(mytest);
+ * }
+ * ```
+ */
 #define MU_RUN_SUITE(suiteName) MU__SAFE_BLOCK(\
     suiteName();\
     minunitSetup = NULL;\
     minunitTeardown = NULL;\
 )
 
-/*  Configure setup and teardown functions */
-#define MU_SUITE_CONFIGURE(setupFun, teardownFun) MU__SAFE_BLOCK(\
-    minunitSetup = setupFun;\
-    minunitTeardown = teardownFun;\
-)
-
-/*  Test runner */
+/**
+ * \brief executes a test
+ * 
+ * Runs the test, the test setup (if configured) will be run before and
+ * test teardown (if configured) will be executed afterwards.
+ * ```
+ * MU_TEST_SUITE(mysuite)
+ * {
+ *     MU_SUITE_CONFIGURE(&myTestSetup, &myTestTeardown);
+ *     MU_RUN_TEST(mytest); // clears the setup and teardown
+ *     MU_SUITE_CONFIGURE(&myTestSetup, &myTestTeardown);
+ *     MU_RUN_TEST(mytest2);
+ * }
+ * ```
+ * @param[in] test Test name to execute
+ */
 #define MU_RUN_TEST(test) MU__SAFE_BLOCK(\
     if (minunitSetup) (*minunitSetup)();\
     minunitStatus = 0;\
@@ -83,7 +137,32 @@ extern int minunitAsserts; /*!< Total test asserts executed, needs to be globall
     if (minunitTeardown) (*minunitTeardown)();\
 )
 
-/*  Assertions */
+/**
+ * \brief Creates a test
+ * 
+ * Creates a test.
+ * ```
+ * MU_TEST(myTest)
+ * {
+ *     mu_check(true == true);
+ * }
+ * ```
+ * @param[in] methodName Test name
+ */
+#define MU_TEST(methodName) static void methodName(void)
+
+/**
+ * \brief Executes a check
+ * 
+ * Executes a check, the argument needs to be true for the check to pass.
+ * ```
+ * MU_TEST(myTest)
+ * {
+ *     mu_check(true == true);
+ * }
+ * ```
+ * @param[in] test Check to perform
+ */
 #define mu_check(test) MU__SAFE_BLOCK(\
     minunitAsserts++;\
     if (!(test)) {\
