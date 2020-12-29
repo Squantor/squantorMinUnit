38543d51376e17acaab1c252039a57b7f95ca00c
Squantor <squantmuts@gmail.com>
2020-12-29 15:07:46 +0100

    added define for max amount of tests

--- src/MinUnit.c
+++ src/MinUnit.c
@@ -1,13 +1,21 @@
 #include <MinUnit.h>
 
-void (*minunitTestsTable[1000])(void);
+#ifndef MINUNIT_MAX_TESTS
+#warning No maximum number of tests defined, defaulting to 100
+#define MINUNIT_MAX_TESTS 100
+#endif
+
+void (*minunitTestsTable[MINUNIT_MAX_TESTS])(void);
 int minunitTestCount = 0;
 
 
 void minunitAddTest(const char *name, void (*autoreg_func)(void))
 {
-    minunitTestsTable[minunitTestCount] = autoreg_func;
-    minunitTestCount++;
+    if(minunitTestCount < MINUNIT_MAX_TESTS-1)
+    {
+        minunitTestsTable[minunitTestCount] = autoreg_func;
+        minunitTestCount++;
+    }
 }
 
 int minunitRun(void)
