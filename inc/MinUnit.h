bf90b44f87302201cd570f5a6fded79efc2b547f
Squantor <squantmuts@gmail.com>
2022-04-15 13:18:08 +0200

    added callback when a check fails

--- inc/MinUnit.h
+++ inc/MinUnit.h
@@ -4,25 +4,25 @@
  * Copyright (c) 2020-2020 Bart Bilos
  * For conditions of distribution and use, see LICENSE file
  */
-/** 
+/**
  * \file MinUnit.hpp
- * 
+ *
  * Minimal C++ unittesting framework
- * 
+ *
  * Minimal C++ unittesting framework, based on:
  * https://github.com/siu/minunit
  * Further inspiration drawn from:
  * https://github.com/ollelogdahl/ihct
- */ 
+ */
 #ifndef MINUNIT_H
 #define MINUNIT_H
 
 #ifndef NULL
-# ifndef __cplusplus
-#  define NULL ((void *)0)
-# else
-# define NULL 0
-# endif
+#ifndef __cplusplus
+#define NULL ((void *)0)
+#else
+#define NULL 0
+#endif
 #endif
 
 #ifdef __cplusplus
@@ -38,27 +38,27 @@ extern "C" {
  * \brief Datastructure defining test state
  */
 typedef struct {
-    int executed; /*!< Total tests executed */
-    int failures; /*!< Total tests failed */
-    int checks; /*!< Total test asserts executed */
-    int flagFailed; /*!< did the current test fail */
+  int executed;   /*!< Total tests executed */
+  int failures;   /*!< Total tests failed */
+  int checks;     /*!< Total test asserts executed */
+  int flagFailed; /*!< did the current test fail */
 } minunitState;
 
 /**
  * \brief Test instance entry
  */
 typedef struct {
-    void (*testBody)(minunitState *testResults); /*!< code to test */
-    void (*testSetup)(minunitState *testResults); /*!< setup to run before the test */
-    void (*testTeardown)(minunitState *testResults); /*!< teardown to run after the test */
+  void (*testBody)(minunitState *testResults);     /*!< code to test */
+  void (*testSetup)(minunitState *testResults);    /*!< setup to run before the test */
+  void (*testTeardown)(minunitState *testResults); /*!< teardown to run after the test */
 } minunitTestEntry;
 
 /**
  * \brief Table of tests
  */
 typedef struct {
-    int testcount;  /*!< amount of tests in the table */
-    minunitTestEntry tests[MINUNIT_MAX_TESTS]; /*!< table of tests */
+  int testcount;                             /*!< amount of tests in the table */
+  minunitTestEntry tests[MINUNIT_MAX_TESTS]; /*!< table of tests */
 } minunitTestsTable;
 
 /**
@@ -68,125 +68,130 @@ typedef void (*intraTestCallback)(void);
 
 /**
  * \brief Macro to define a test setup function
- * 
+ *
  * Helper macro to create a function definition of a test setup
  * @param[in]  name         Name of the test setup function
  */
-#define MINUNIT_SETUP(name)\
-    void name(minunitState *testResults)
+#define MINUNIT_SETUP(name) void name(minunitState *testResults)
 
 /**
  * \brief Macro to define a test teardown function
- * 
+ *
  * Helper macro to create a function definition of a test teardown
  * @param[in]  name         Name of the test teardown function
  */
-#define MINUNIT_TEARDOWN(name)\
-    void name(minunitState *testResults)
+#define MINUNIT_TEARDOWN(name) void name(minunitState *testResults)
 
-
-extern minunitState minunitTestState; /*!< minunit global state */
+extern minunitState minunitTestState;      /*!< minunit global state */
 extern minunitTestsTable defaultTestTable; /*!< default table of tests */
 
 /**
  * \brief Macro to register a test
- * 
+ *
  * Helper macro to register a test with minunit
  * @param[in]  name         Name of the test
  * @param[in]  setupFunc    test setup function, NULL for no setup
  * @param[in]  teardownFunc test setup function, NULL for no setup
  */
-#define MINUNIT_ADD(name, setupFunc, teardownFunc)\
-    static void minunit_##name(minunitState *testResults);\
-    static void __attribute__((constructor)) __construct_minunit_##name(void) {\
-        minunitAddTest(&defaultTestTable, minunit_##name, setupFunc, teardownFunc);\
-    }\
-    static void minunit_##name(minunitState *testResults)
+#define MINUNIT_ADD(name, setupFunc, teardownFunc)                              \
+  static void minunit_##name(minunitState *testResults);                        \
+  static void __attribute__((constructor)) __construct_minunit_##name(void) {   \
+    minunitAddTest(&defaultTestTable, minunit_##name, setupFunc, teardownFunc); \
+  }                                                                             \
+  static void minunit_##name(minunitState *testResults)
 
 /**
  * \brief function to add test to the test table
- * 
+ *
  * Used internally by the macro MINUNIT_ADD
- * 
+ *
  * @param[in]  table        Pointer to a table of tests
  * @param[in]  testBody     Function pointer to the actual test
- * @param[in]  testSetup    Function pointer to the test setup 
+ * @param[in]  testSetup    Function pointer to the test setup
  * @param[in]  testTeardown Function pointer to the test teardown
  */
-void minunitAddTest(
-    minunitTestsTable *table,
-    void (*testBody)(minunitState *testResults),
-    void (*testSetup)(minunitState *testResults),
-    void (*testTeardown)(minunitState *testResults));
+void minunitAddTest(minunitTestsTable *table, void (*testBody)(minunitState *testResults),
+                    void (*testSetup)(minunitState *testResults), void (*testTeardown)(minunitState *testResults));
 
 /**
  * \brief function execute all tests
- * 
+ *
  * Executes all tests registered with MINUNIT_ADD
  */
 int minunitRun(void);
 
 /**
  * \brief function execute all tests with a callback inbetween
- * 
+ *
  * Executes all tests registered with MINUNIT_ADD and calls between each test
  * a callback given by the callback argument.
- * 
+ *
  * @param[in]   callback    Function pointer to call in between tests
  */
 int minunitRunCallback(intraTestCallback callback);
 
 /**
  * \brief function execute all tests with a in between test callback from a given table
- * 
+ *
  * Executes all tests passed along in the tests table and calls between each test
  * a callback given by the callback argument.
- * 
+ *
  * @param[in]   table       Table of tests to execute
  * @param[in]   callback    Function pointer to call in between tests
  */
 int minunitRunTableCallback(minunitTestsTable *table, intraTestCallback callback);
 
 /**
- * \brief function to report messages
- * 
- * Used by minunit to report failing tests or just print progress
- * 
+ * \brief weak overridable callback function to report messages
+ *
+ * Used by minunit to report failing tests or just print progress. This is a weak function
+ * to be overridden by the user if he wants a different report message
+ *
  * @param[in]  message   Message to print
  */
-void minunitReport(const char * message);
+void minunitReport(const char *message);
+
+/**
+ * \brief weak overridable callback function called when a test fails
+ *
+ * When a minunit check fails, this function is called. Useful for examining the callstack in embedded
+ * targets to see where it fails.
+ *
+ */
+void minunitFailCallback();
 
 /**
  * \brief Macro to wrap a safe define block
- * 
+ *
  * Helper macro to create a safe block to be used in other C style macros
  * @param[in]  block   Block of statements that will be wrapped in a ```do{ }while(0)``` block.
  */
-#define MU__SAFE_BLOCK(block) do {\
-    block\
-} while(0)
+#define MU__SAFE_BLOCK(block) \
+  do {                        \
+    block                     \
+  } while (0)
 
 /**
  * \brief Convert a macro literal to string
- * 
+ *
  * Helper macro to convert a macro literal to a string, useful for __LINE__
- * 
+ *
  * @param[in]  x   Macro definition to convert to a string
  */
 #define STRINGIFY(x) #x
 
 /**
  * \brief Convert a macro literal to string
- * 
+ *
  * Final helper macro to convert a macro literal to a string, useful for __LINE__
- * 
+ *
  * @param[in]  x   Macro definition to convert to a string
  */
 #define TOSTRING(x) STRINGIFY(x)
 
 /**
  * \brief Executes a check
- * 
+ *
  * Executes a check, the argument needs to be true for the check to pass.
  * ```
  * MU_TEST(myTest)
@@ -198,45 +203,34 @@ void minunitReport(const char * message);
  */
 
 #ifndef MINUNIT_REPORT_DISABLE
-#define minUnitCheck(test) MU__SAFE_BLOCK(\
-    testResults->checks++;\
-    if (!(test)) {\
-        testResults->flagFailed = 1;\
-        minunitReport("\n" __FILE__ ":" TOSTRING(__LINE__) " failed: " TOSTRING(test) "\n");\
-        return;\
-    }\
-    else\
-    {\
-        minunitReport(".");\
-    }\
-)
+#define minUnitCheck(test)                                                                 \
+  MU__SAFE_BLOCK(                                                                          \
+    testResults->checks++; if (!(test)) {                                                  \
+      minunitFailCallback();                                                               \
+      testResults->flagFailed = 1;                                                         \
+      minunitReport("\n" __FILE__ ":" TOSTRING(__LINE__) " failed: " TOSTRING(test) "\n"); \
+      return;                                                                              \
+    } else { minunitReport("."); })
 #else
-#define minUnitCheck(test) MU__SAFE_BLOCK(\
-    testResults->checks++;\
-    if (!(test)) {\
-        testResults->flagFailed = 1;\
-        return;\
-    }\
-)
+#define minUnitCheck(test)                             \
+  MU__SAFE_BLOCK(testResults->checks++; if (!(test)) { \
+    minunitFailCallback();                             \
+    testResults->flagFailed = 1;                       \
+    return;                                            \
+  })
 #endif
 
 /**
  * \brief automatically pass test
- * 
+ *
  * Automatically passing test, useful for the teardown and setup functions
  */
-#define minUnitPass() MU__SAFE_BLOCK(\
-    testResults->checks++;\
-)
+#define minUnitPass() MU__SAFE_BLOCK(testResults->checks++;)
 
 /**
  * \brief automatically fail test
  */
-#define minUnitFail() MU__SAFE_BLOCK(\
-    testResults->checks++;\
-    testResults->flagFailed = 1;\
-    return;\
-)
+#define minUnitFail() MU__SAFE_BLOCK(testResults->checks++; testResults->flagFailed = 1; return;)
 
 #ifdef __cplusplus
 }
--- src/MinUnit.c
+++ src/MinUnit.c
@@ -4,64 +4,51 @@ minunitTestsTable defaultTestTable = {0};
 
 minunitState minunitTestState;
 
-void minunitAddTest( minunitTestsTable *table,
-    void (*testBody)(minunitState *testResults),
-    void (*testSetup)(minunitState *testResults),
-    void (*testTeardown)(minunitState *testResults))
-{
-    if(table->testcount < MINUNIT_MAX_TESTS-1)
-    {
-        table->tests[table->testcount].testBody = testBody;
-        table->tests[table->testcount].testSetup = testSetup;
-        table->tests[table->testcount].testTeardown = testTeardown;
-        table->testcount++;
+void minunitAddTest(minunitTestsTable *table, void (*testBody)(minunitState *testResults),
+                    void (*testSetup)(minunitState *testResults), void (*testTeardown)(minunitState *testResults)) {
+  if (table->testcount < MINUNIT_MAX_TESTS - 1) {
+    table->tests[table->testcount].testBody = testBody;
+    table->tests[table->testcount].testSetup = testSetup;
+    table->tests[table->testcount].testTeardown = testTeardown;
+    table->testcount++;
+  }
+}
+
+int minunitRun(void) {
+  return minunitRunTableCallback(&defaultTestTable, NULL);
+}
+
+int minunitRunCallback(intraTestCallback callback) {
+  return minunitRunTableCallback(&defaultTestTable, callback);
+}
+
+int minunitRunTableCallback(minunitTestsTable *table, intraTestCallback callback) {
+  minunitTestState.executed = 0;
+  minunitTestState.failures = 0;
+  minunitTestState.checks = 0;
+  for (int i = 0; i < table->testcount; i++) {
+    if (table->tests[i].testSetup != NULL) table->tests[i].testSetup(&minunitTestState);
+    if (minunitTestState.flagFailed == 0) {
+      table->tests[i].testBody(&minunitTestState);
+      if (table->tests[i].testTeardown != NULL) table->tests[i].testTeardown(&minunitTestState);
     }
-}
-
-int minunitRun(void) 
-{
-    return minunitRunTableCallback(&defaultTestTable, NULL);
-}
-
-int minunitRunCallback(intraTestCallback callback)
-{
-    return minunitRunTableCallback(&defaultTestTable, callback);
-}
-
-int minunitRunTableCallback(minunitTestsTable *table, intraTestCallback callback)
-{
-    minunitTestState.executed = 0;
-    minunitTestState.failures = 0;
-    minunitTestState.checks = 0;
-    for(int i = 0; i < table->testcount; i++)
-    {
-        if(table->tests[i].testSetup != NULL)
-            table->tests[i].testSetup(&minunitTestState);
-        if(minunitTestState.flagFailed == 0)
-        {
-            table->tests[i].testBody(&minunitTestState);
-            if(table->tests[i].testTeardown != NULL)
-                table->tests[i].testTeardown(&minunitTestState);  
-        }
-        if(minunitTestState.flagFailed != 0)
-        {
-            minunitTestState.flagFailed = 0;
-            minunitTestState.failures++;
-        }
-        minunitTestState.executed++;
-        if(callback != NULL)
-            callback();
-        #ifndef MINUNIT_REPORT_DISABLE
-        minunitReport("\n");
-        #endif
+    if (minunitTestState.flagFailed != 0) {
+      minunitTestState.flagFailed = 0;
+      minunitTestState.failures++;
     }
-    return 0;
+    minunitTestState.executed++;
+    if (callback != NULL) callback();
+#ifndef MINUNIT_REPORT_DISABLE
+    minunitReport("\n");
+#endif
+  }
+  return 0;
 }
-
 
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 
-__attribute__((weak)) void minunitReport(const char * message)
-{
-    // empty body, as this is a weak function, no action should be taken
+__attribute__((weak)) void minunitReport(const char *message) {
+  // empty body, as this is a weak function, no action should be taken
 }
+
+__attribute__((weak)) void minunitFailCallback() {}
